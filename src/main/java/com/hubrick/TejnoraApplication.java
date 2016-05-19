e8ff06b29c1ccb83a95cbb8c58827e03e3eeab59
Karel Tejnora <karel@tejnora.cz>
2016-05-19 21:53:16 +0200

    Initial config

---
+++ src/main/java/com/hubrick/TejnoraApplication.java
@@ -0,0 +1,12 @@
+package com.hubrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TejnoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TejnoraApplication.class, args);
+	}
+}
